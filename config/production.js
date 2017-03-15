b1c8642f745945792958e8804c642c5af8d84687
VampireKnigh <245799060@qq.com>
2017-03-15 10:27:04 +0800

    myblog

---
+++ config/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port:80,
+    mongodb: 'mongodb://myblog:myblog@ds131340.mlab.com:31340/myblog'
+};
---
+++ models/user.js
@@ -0,0 +1,14 @@
+var User = require('../lib/mongo').User;
+
+module.exports = {
+    // 注册一个用户
+    create:function create(user){
+        return User.create(user).exec();
+    },
+    // 通过用户名获取用户信息
+    getUserByName:function getUserByName(name){
+        //addCreatedAt 自定义插件（通过 _id 生成时间戳）{
+        return User.findOne({name:name}).addCreatedAt().exec();
+    }
+
+};
